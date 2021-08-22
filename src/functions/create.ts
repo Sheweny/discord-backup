2ec1d4ded461aeff2c82159d6f379f43bb9719f6
Raphael <veauville.raphael@gmail.com>
2021-08-22 21:01:21 +0200

    Fix

--- src/functions/create.ts
+++ src/functions/create.ts
@@ -1,115 +1,119 @@
 import { Guild, SnowflakeUtil } from "discord.js";
-import { GuildDB } from '../models/index';
-import { Types } from 'mongoose';
+import { GuildDB } from "../models/index";
+import { Types } from "mongoose";
 
 export async function create(guild: Guild) {
-	const bans = await fetchBans(guild)
-	const categories = await fetchCategories(guild)
-	const channels = await fetchChannels(guild)
-	const emojis = await fetchEmojis(guild)
-	const roles = await fetchRoles(guild)
+  const bans = await fetchBans(guild);
+  const categories = await fetchCategories(guild);
+  const channels = await fetchChannels(guild);
+  const emojis = await fetchEmojis(guild);
+  const roles = await fetchRoles(guild);
 
-	const backupId = SnowflakeUtil.generate(Date.now())
-	const merged = Object.assign({
-		_id: Types.ObjectId(),
-		backupID: backupId,
-		gName: guild.name,
-		gID: guild.id,
-		gDescription: guild.description,
-		gIcon: guild.iconURL(),
-		gBans: bans,
-		gCategories: categories,
-		gChannels: channels,
-		gEmojis: emojis,
-		gRoles: roles
-	});
-	const createGuild = new GuildDB(merged);
-	await createGuild.save();
-	return backupId;
+  const backupId = SnowflakeUtil.generate(Date.now());
+  const merged = Object.assign({
+    _id: Types.ObjectId(),
+    backupID: backupId,
+    gName: guild.name,
+    gID: guild.id,
+    gDescription: guild.description,
+    gIcon: guild.iconURL(),
+    gBans: bans,
+    gCategories: categories,
+    gChannels: channels,
+    gEmojis: emojis,
+    gRoles: roles,
+  });
+  const createGuild = new GuildDB(merged);
+  await createGuild.save();
+  return backupId;
 }
 
 async function fetchBans(guild: Guild) {
-	const bans: string[] = [];
-	const bansFetch = await guild.bans.fetch()
-	bansFetch.each(b => {
-		bans.push(b.user.id)
-	})
-	return bans
+  const bans: string[] = [];
+  const bansFetch = await guild.bans.fetch();
+  bansFetch.each((b: any) => {
+    bans.push(b.user.id);
+  });
+  return bans;
 }
 
 async function fetchCategories(guild: Guild) {
-	const categories: any[] = [];
-	const channelsFetch = await guild.channels.fetch()
-	const categoriesFilter = channelsFetch.filter(c => c.type === 'GUILD_CATEGORY')
-	for (const [_, cat] of categoriesFilter) {
-		const permissions: any[] = []
-		for (const p of cat.permissionOverwrites.cache.values()) {
-			permissions.push({
-				id: p.id,
-				type: p.type,
-				deny: p.deny.bitfield.toString(),
-				allow: p.allow.bitfield.toString()
-			})
-		}
-		categories.push({
-			name: cat.name,
-			rawPosition: cat.rawPosition,
-			permissionOverwrites: permissions
-		})
-	}
-	return categories
+  const categories: any[] = [];
+  const channelsFetch = await guild.channels.fetch();
+  const categoriesFilter = channelsFetch.filter(
+    (c: any) => c.type === "GUILD_CATEGORY"
+  );
+  for (const [_, cat] of categoriesFilter) {
+    const permissions: any[] = [];
+    for (const p of cat.permissionOverwrites.cache.values()) {
+      permissions.push({
+        id: p.id,
+        type: p.type,
+        deny: p.deny.bitfield.toString(),
+        allow: p.allow.bitfield.toString(),
+      });
+    }
+    categories.push({
+      name: cat.name,
+      rawPosition: cat.rawPosition,
+      permissionOverwrites: permissions,
+    });
+  }
+  return categories;
 }
 
 async function fetchChannels(guild: Guild) {
-	const channels: any[] = [];
-	const channelsFetch = await guild.channels.fetch()
-	const channelsFilter: any = channelsFetch.filter(c => c.type !== 'GUILD_CATEGORY')
-	for (const [_, cha] of channelsFilter) {
-		const permissions: any[] = []
-		for (const p of cha.permissionOverwrites.cache.values()) {
-			permissions.push({
-				id: p.id,
-				type: p.type,
-				deny: p.deny.bitfield.toString(),
-				allow: p.allow.bitfield.toString()
-			})
-		}
-		channels.push({
-			name: cha.name,
-			type: cha.type,
-			rawPosition: cha.rawPosition,
-			parent: cha.parent.name,
-			permissionOverwrites: permissions,
-			topic: cha.topic || '',
-			nsfw: cha.nsfw || false,
-		})
-	}
-	return channels;
+  const channels: any[] = [];
+  const channelsFetch = await guild.channels.fetch();
+  const channelsFilter: any = channelsFetch.filter(
+    (c: any) => c.type !== "GUILD_CATEGORY"
+  );
+  for (const [_, cha] of channelsFilter) {
+    const permissions: any[] = [];
+    for (const p of cha.permissionOverwrites.cache.values()) {
+      permissions.push({
+        id: p.id,
+        type: p.type,
+        deny: p.deny.bitfield.toString(),
+        allow: p.allow.bitfield.toString(),
+      });
+    }
+    channels.push({
+      name: cha.name,
+      type: cha.type,
+      rawPosition: cha.rawPosition,
+      parent: cha.parent?.name,
+      permissionOverwrites: permissions,
+      topic: cha.topic || "",
+      nsfw: cha.nsfw || false,
+    });
+  }
+  return channels;
 }
 
 async function fetchEmojis(guild: Guild) {
-	const emojisFetch = await guild.emojis.fetch();
-	const emojis: any[] = []
-	for (const [_, emot] of emojisFetch) {
-		emojis.push({
-			name: emot.name,
-			animated: emot.animated,
-			id: emot.id
-		})
-	}
-	return emojis
+  const emojisFetch = await guild.emojis.fetch();
+  const emojis: any[] = [];
+  for (const [_, emot] of emojisFetch) {
+    emojis.push({
+      name: emot.name,
+      animated: emot.animated,
+      id: emot.id,
+    });
+  }
+  return emojis;
 }
 
 async function fetchRoles(guild: Guild) {
-	const roles: any[] = []
-	const rolesFetch = await guild.roles.fetch();
-	for (const [_, r] of rolesFetch) {
-		roles.push({
-			name: r.name,
-			color: r.color,
-			rawPosition: r.rawPosition,
-			permissions: r.permissions.bitfield.toString(),
-		})
-	}
-	return roles;
+  const roles: any[] = [];
+  const rolesFetch = await guild.roles.fetch();
+  for (const [_, r] of rolesFetch) {
+    roles.push({
+      name: r.name,
+      color: r.color,
+      rawPosition: r.rawPosition,
+      permissions: r.permissions.bitfield.toString(),
+    });
+  }
+  return roles;
 }
--- src/functions/load.ts
+++ src/functions/load.ts
@@ -1,116 +1,116 @@
 import type { Guild } from "discord.js";
 import type { IBackup, IObject } from "../typescript/interfaces";
-import { GuildDB } from '../models/index';
-import { wait } from '../util/wait';
+import { GuildDB } from "../models/index";
+import { wait } from "../util/wait";
 
 export async function load(guild: Guild, backupId: string) {
-	const guildDB = await GuildDB.findOne({ backupID: backupId })
+  const guildDB = await GuildDB.findOne({ backupID: backupId });
 
-	if (!guildDB) throw new Error('Could not find guild backup ' + backupId);
-	await prepar(guild)
-	await loadRoles(guild, guildDB)
-	await loadCategories(guild, guildDB)
-	await loadChannels(guild, guildDB)
-	await loadEmojis(guild, guildDB)
-	await loadBans(guild, guildDB)
-	return true
+  if (!guildDB) throw new Error("Could not find guild backup " + backupId);
+  await prepar(guild);
+  await loadRoles(guild, guildDB);
+  await loadCategories(guild, guildDB);
+  await loadChannels(guild, guildDB);
+  await loadEmojis(guild, guildDB);
+  await loadBans(guild, guildDB);
+  return true;
 }
 
-
 async function prepar(guild: Guild): Promise<void> {
-	// Fetch
-	const g = await guild.fetch()
-	// Delete all
+  // Fetch
+  const g = await guild.fetch();
+  // Delete all
 
-	//Roles
-	for (const role of g.roles.cache.values()) {
-		try {
-			if (role.id !== role.guild.id && role.editable) {
-				if (g.me!.roles.highest.comparePositionTo(role) <= 0) continue;
-				await role.delete();
-				await wait(100);
-			}
-		} catch (e) {
-		}
-	};
-	// Channels 
-	for (const channel of g.channels.cache.values()) {
-		try {
-			if (!channel.isThread() && channel.deletable) {
-				await channel.delete();
-				await wait(100);
-			}
-		} catch (e) {
-		}
-	};
-	// Emojis
-	for (const emoji of g.emojis.cache.values()) {
-		try {
-			if (emoji.deletable) {
-				await emoji.delete();
-				await wait(100);
-			}
-		} catch (e) {
-		}
-	};
+  //Roles
+  for (const role of g.roles.cache.values()) {
+    try {
+      if (role.id !== role.guild.id && role.editable) {
+        if (g.me!.roles.highest.comparePositionTo(role) <= 0) continue;
+        await role.delete();
+        await wait(100);
+      }
+    } catch (e) {}
+  }
+  // Channels
+  for (const channel of g.channels.cache.values()) {
+    try {
+      if (!channel.isThread() && channel.deletable) {
+        await channel.delete();
+        await wait(100);
+      }
+    } catch (e) {}
+  }
+  // Emojis
+  for (const emoji of g.emojis.cache.values()) {
+    try {
+      if (emoji.deletable) {
+        await emoji.delete();
+        await wait(100);
+      }
+    } catch (e) {}
+  }
 }
 
 async function loadCategories(guild: Guild, backup: IBackup) {
+  for (const cat of backup.gCategories) {
+    await wait(100);
+    cat.permissionOverwrites.forEach((p: IObject) => {
+      p.allow = BigInt(p.allow);
+      p.deny = BigInt(p.deny);
+    });
 
-	for (const cat of backup.gCategories) {
-		await wait(100)
-		cat.permissionOverwrites.forEach((p: IObject) => {
-			p.allow = BigInt(p.allow)
-			p.deny = BigInt(p.deny)
-		})
-
-		await guild.channels.create(cat.name, {
-			type: 'GUILD_CATEGORY',
-			position: cat.rawPosition,
-			permissionOverwrites: cat.permissionOverwrites
-		})
-	}
+    await guild.channels.create(cat.name, {
+      type: "GUILD_CATEGORY",
+      position: cat.rawPosition,
+      permissionOverwrites: cat.permissionOverwrites,
+    });
+  }
 }
 async function loadChannels(guild: Guild, backup: IBackup) {
-	for (const ch of backup.gChannels) {
-		const parent: any = guild.channels.cache.filter(c => c.type === 'GUILD_CATEGORY' && c.name === ch.parent).first()
-		await wait(100)
-		ch.permissionOverwrites.forEach((p: IObject) => {
-			p.allow = BigInt(p.allow)
-			p.deny = BigInt(p.deny)
-		})
-		await guild.channels.create(ch.name, {
-			type: ch.type,
-			topic: ch.topic,
-			position: ch.rawPosition,
-			permissionOverwrites: ch.permissionOverwrites,
-			parent: parent
-		})
-	}
+  for (const ch of backup.gChannels) {
+    const parent: any = guild.channels.cache
+      .filter((c: any) => c.type === "GUILD_CATEGORY" && c.name === ch.parent)
+      ?.first();
+    await wait(100);
+    ch.permissionOverwrites.forEach((p: IObject) => {
+      p.allow = BigInt(p.allow);
+      p.deny = BigInt(p.deny);
+    });
+    await guild.channels.create(ch.name, {
+      type: ch.type,
+      topic: ch.topic,
+      position: ch.rawPosition,
+      permissionOverwrites: ch.permissionOverwrites,
+      parent: parent,
+    });
+  }
 }
 
 async function loadRoles(guild: Guild, backup: IBackup) {
-	for (const r of backup.gRoles) {
-		await wait(100)
-		await guild.roles.create({
-			name: r.name,
-			color: r.color,
-			position: r.position,
-			permissions: BigInt(r.permissions)
-		})
-	}
+  for (const r of backup.gRoles) {
+    await wait(100);
+    await guild.roles.create({
+      name: r.name,
+      color: r.color,
+      position: r.position,
+      permissions: BigInt(r.permissions),
+    });
+  }
 }
 
 async function loadEmojis(guild: Guild, backup: IBackup) {
-	for (const e of backup.gEmojis) {
-		await wait(100)
-		await guild.emojis.create(`https://cdn.discordapp.com/emojis/${e.id}.${e.animated ? 'gif' : 'png'}`, e.name)
-	}
+  for (const e of backup.gEmojis) {
+    await wait(100);
+    await guild.emojis.create(
+      `https://cdn.discordapp.com/emojis/${e.id}.${e.animated ? "gif" : "png"}`,
+      e.name
+    );
+  }
 }
 
 async function loadBans(guild: Guild, backup: IBackup) {
-	for (const b of backup.gBans) {
-		await wait(100)
-		await guild.bans.create(b, { reason: 'Backup' })
-	}
+  for (const b of backup.gBans) {
+    await wait(100);
+    await guild.bans.create(b, { reason: "Backup" });
+  }
 }
